f8d09cde240cbb2295936b3d0f08e89c8d43b357
AdityaKG-169 <f20190338@goa.bits-pilani.ac.in>
2020-10-02 18:28:39 +0530

    Yet to add searchbox, extension link, edit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./app.css";
+import Navbar from "./Components/Navbar/Navbar";
+import HomePage from "./Pages/HomePage/HomePage";
+import PageNotFound from "./Pages/PageNotFound/PageNotFound";
+import ProfilePage from "./Pages/ProfilePage/ProfilePage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Navbar />
+			<Switch>
+				<Route component={HomePage} exact path="/" />
+				<Route component={ProfilePage} path="/profile" />
+				<Route component={PageNotFound} path="*" />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
